5cb9fa485e6f51426790a9594f2bf9cff8af8e5c
Michelle Wiraydi <miwiryadi@gmail.com>
2017-12-28 18:47:53 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,18 @@
 class Person
-  def initialize(name, job)
+  def name
+    @name
+  end
+
+  def name=(name)
     @name = name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(job)
     @job = job
   end
-end
 
-beyonce = Person.new("beyonce")
+end
